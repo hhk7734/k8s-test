bedc67e648c84182a580db776812b3c9db0ed564
Hyeonki Hong <hhk7734@gmail.com>
2021-10-01 00:24:20 +0900

    pulumi: master: update

--- pulumi/master/__main__.py
+++ pulumi/master/__main__.py
@@ -3,9 +3,15 @@ import json
 import pulumi
 import pulumi_aws as aws
 
+# 10.234.0.0/16
+# 10.234.0.0 네트워크 주소
+# 10.234.0.1 VPC 라우터 주소
+# 10.234.0.2 DNS 서버 주소
+# 10.234.0.3 Reserved
+# 10.234.255.255 Reserved
 k8s_vpc = aws.ec2.Vpc(
     "k8sVpc",
-    cidr_block="10.240.0.0/16",
+    cidr_block="10.234.0.0/16",
     tags={
         "Name": "k8sVpc",
         "Owner": "hhk7734",
@@ -15,19 +21,18 @@ k8s_vpc = aws.ec2.Vpc(
 k8s_subnet_a = aws.ec2.Subnet(
     "k8sSubnetA",
     vpc_id=k8s_vpc.id,
-    cidr_block="10.240.1.0/24",
+    cidr_block="10.234.1.0/24",
     tags={
         "Name": "k8sSubnetA",
         "Owner": "hhk7734",
     },
 )
 
-k8s_master_0_ni = aws.ec2.NetworkInterface(
-    "k8sMaster0NI",
-    subnet_id=k8s_subnet_a.id,
-    private_ips=["10.240.1.11"],
+k8s_igw = aws.ec2.InternetGateway(
+    "k8sIGW",
+    vpc_id=k8s_vpc.id,
     tags={
-        "Name": "k8sMaster0NI",
+        "Name": "k8sIGW",
         "Owner": "hhk7734",
     },
 )
@@ -36,15 +41,11 @@ k8s_master_0 = aws.ec2.Instance(
     "k8sMaster0",
     ami="ami-090717c950a5c34d3",  # Ubuntu Server 18.04 LTS
     instance_type="t3.medium",
+    associate_public_ip_address=True,
+    subnet_id=k8s_subnet_a.id,
     credit_specification=aws.ec2.InstanceCreditSpecificationArgs(
         cpu_credits="unlimited",
     ),
-    network_interfaces=[
-        aws.ec2.InstanceNetworkInterfaceArgs(
-            network_interface_id=k8s_master_0_ni.id,
-            device_index=0,
-        )
-    ],
     tags={
         "Name": "k8sMaster0",
         "Owner": "hhk7734",
