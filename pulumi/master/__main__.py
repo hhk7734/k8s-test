05e93aa538d284ca9d888c5aefea5396b489ac21
Hyeonki Hong <hhk7734@gmail.com>
2021-10-29 11:22:40 +0900

    pulumi: master: extract policy to file

--- pulumi/master/__main__.py
+++ pulumi/master/__main__.py
@@ -6,6 +6,8 @@ import pulumi_aws as aws
 
 stack = pulumi.get_stack()
 
+BASE_DIR = Path(__file__).parent.resolve()
+
 # 10.234.0.0/16
 # 10.234.0.0 네트워크 주소
 # 10.234.0.1 VPC 라우터 주소
@@ -300,105 +302,22 @@ k8s_alb_http_listener = aws.lb.Listener(
     },
 )
 
+with BASE_DIR.joinpath("iam", "policy", "master.json").open() as fd:
+    master_policy = fd.read()
 
 k8s_master_iam_policy = aws.iam.RolePolicy(
     "k8s_master_iam_policy",
     role=k8s_master_iam_role.id,
-    policy=json.dumps(
-        {
-            "Version": "2012-10-17",
-            "Statement": [
-                {
-                    "Effect": "Allow",
-                    "Action": [
-                        "autoscaling:DescribeAutoScalingGroups",
-                        "autoscaling:DescribeLaunchConfigurations",
-                        "autoscaling:DescribeTags",
-                        "ec2:DescribeInstances",
-                        "ec2:DescribeRegions",
-                        "ec2:DescribeRouteTables",
-                        "ec2:DescribeSecurityGroups",
-                        "ec2:DescribeSubnets",
-                        "ec2:DescribeVolumes",
-                        "ec2:CreateSecurityGroup",
-                        "ec2:CreateTags",
-                        "ec2:CreateVolume",
-                        "ec2:ModifyInstanceAttribute",
-                        "ec2:ModifyVolume",
-                        "ec2:AttachVolume",
-                        "ec2:AuthorizeSecurityGroupIngress",
-                        "ec2:CreateRoute",
-                        "ec2:DeleteRoute",
-                        "ec2:DeleteSecurityGroup",
-                        "ec2:DeleteVolume",
-                        "ec2:DetachVolume",
-                        "ec2:RevokeSecurityGroupIngress",
-                        "ec2:DescribeVpcs",
-                        "elasticloadbalancing:AddTags",
-                        "elasticloadbalancing:AttachLoadBalancerToSubnets",
-                        "elasticloadbalancing:ApplySecurityGroupsToLoadBalancer",
-                        "elasticloadbalancing:CreateLoadBalancer",
-                        "elasticloadbalancing:CreateLoadBalancerPolicy",
-                        "elasticloadbalancing:CreateLoadBalancerListeners",
-                        "elasticloadbalancing:ConfigureHealthCheck",
-                        "elasticloadbalancing:DeleteLoadBalancer",
-                        "elasticloadbalancing:DeleteLoadBalancerListeners",
-                        "elasticloadbalancing:DescribeLoadBalancers",
-                        "elasticloadbalancing:DescribeLoadBalancerAttributes",
-                        "elasticloadbalancing:DetachLoadBalancerFromSubnets",
-                        "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
-                        "elasticloadbalancing:ModifyLoadBalancerAttributes",
-                        "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
-                        "elasticloadbalancing:SetLoadBalancerPoliciesForBackendServer",
-                        "elasticloadbalancing:AddTags",
-                        "elasticloadbalancing:CreateListener",
-                        "elasticloadbalancing:CreateTargetGroup",
-                        "elasticloadbalancing:DeleteListener",
-                        "elasticloadbalancing:DeleteTargetGroup",
-                        "elasticloadbalancing:DescribeListeners",
-                        "elasticloadbalancing:DescribeLoadBalancerPolicies",
-                        "elasticloadbalancing:DescribeTargetGroups",
-                        "elasticloadbalancing:DescribeTargetHealth",
-                        "elasticloadbalancing:ModifyListener",
-                        "elasticloadbalancing:ModifyTargetGroup",
-                        "elasticloadbalancing:RegisterTargets",
-                        "elasticloadbalancing:DeregisterTargets",
-                        "elasticloadbalancing:SetLoadBalancerPoliciesOfListener",
-                        "iam:CreateServiceLinkedRole",
-                        "kms:DescribeKey",
-                    ],
-                    "Resource": ["*"],
-                }
-            ],
-        }
-    ),
+    policy=master_policy,
 )
+
+with BASE_DIR.joinpath("iam", "policy", "worker.json").open() as fd:
+    worker_policy = fd.read()
 
 k8s_worker_iam_policy = aws.iam.RolePolicy(
     "k8s_worker_iam_policy",
     role=k8s_worker_iam_role.id,
-    policy=json.dumps(
-        {
-            "Version": "2012-10-17",
-            "Statement": [
-                {
-                    "Effect": "Allow",
-                    "Action": [
-                        "ec2:DescribeInstances",
-                        "ec2:DescribeRegions",
-                        "ecr:GetAuthorizationToken",
-                        "ecr:BatchCheckLayerAvailability",
-                        "ecr:GetDownloadUrlForLayer",
-                        "ecr:GetRepositoryPolicy",
-                        "ecr:DescribeRepositories",
-                        "ecr:ListImages",
-                        "ecr:BatchGetImage",
-                    ],
-                    "Resource": ["*"],
-                }
-            ],
-        }
-    ),
+    policy=worker_policy,
 )
 
 with Path.home().joinpath(".ssh", "authorized_keys").open() as fd:
