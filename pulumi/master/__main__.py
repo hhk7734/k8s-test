54016df80f6250919f92bf76446cb336ba793915
Hyeonki Hong <hhk7734@gmail.com>
2021-10-30 02:52:19 +0900

    pulumi: master: update tag and remove loadbalancer

--- pulumi/master/__main__.py
+++ pulumi/master/__main__.py
@@ -8,34 +8,39 @@ stack = pulumi.get_stack()
 
 BASE_DIR = Path(__file__).parent.resolve()
 
+common_tags = {
+    "Stack": stack,
+    "kubernetes.io/cluster/hhk-cluster": "owned",  # or "shared"
+    "Owner": "hhk7734",
+}
+
 # 10.234.0.0/16
 # 10.234.0.0 네트워크 주소
 # 10.234.0.1 VPC 라우터 주소
 # 10.234.0.2 DNS 서버 주소
 # 10.234.0.3 Reserved
 # 10.234.255.255 Reserved
+_tags = {"Name": "k8s-vpc"}
+_tags.update(common_tags)
 k8s_vpc = aws.ec2.Vpc(
-    "k8s_vpc",
+    _tags["Name"],
     cidr_block="10.234.0.0/16",
-    tags={
-        "Name": "k8s_vpc",
-        "Stack": stack,
-        "Owner": "hhk7734",
-    },
+    tags=_tags,
 )
 
+_tags = {"Name": "k8s-igw"}
+_tags.update(common_tags)
 k8s_internet_gateway = aws.ec2.InternetGateway(
-    "k8s_internet_gateway",
+    _tags["Name"],
     vpc_id=k8s_vpc.id,
-    tags={
-        "Name": "k8s_internet_gateway",
-        "Stack": stack,
-        "Owner": "hhk7734",
-    },
+    tags=_tags,
 )
 
+
+_tags = {"Name": "k8s-route-table-0"}
+_tags.update(common_tags)
 k8s_route_table_0 = aws.ec2.RouteTable(
-    "k8s_route_table_0",
+    _tags["Name"],
     vpc_id=k8s_vpc.id,
     routes=[
         aws.ec2.RouteTableRouteArgs(
@@ -43,56 +48,38 @@ k8s_route_table_0 = aws.ec2.RouteTable(
             gateway_id=k8s_internet_gateway.id,
         ),  # Public
     ],
-    tags={
-        "Name": "k8s_route_table_0",
-        "Stack": stack,
-        "Owner": "hhk7734",
-    },
+    tags=_tags,
 )
 
 availability_zones = aws.get_availability_zones(state="available")
 
+_tags = {
+    "Name": "k8s-subnet-0",
+    "kubernetes.io/role/elb": "1",
+}
+_tags.update(common_tags)
 k8s_subnet_0 = aws.ec2.Subnet(
-    "k8s_subnet_0",
+    _tags["Name"],
     vpc_id=k8s_vpc.id,
     cidr_block="10.234.1.0/24",
     availability_zone=availability_zones.names[0],
     map_public_ip_on_launch=True,
-    tags={
-        "Name": "k8s_subnet_0",
-        "Stack": stack,
-        "Owner": "hhk7734",
-    },
+    tags=_tags,
 )
 
-k8s_subnet_1 = aws.ec2.Subnet(
-    "k8s_subnet_1",
-    vpc_id=k8s_vpc.id,
-    cidr_block="10.234.128.0/24",
-    availability_zone=availability_zones.names[1],
-    map_public_ip_on_launch=True,
-    tags={
-        "Name": "k8s_subnet_1",
-        "Stack": stack,
-        "Owner": "hhk7734",
-    },
-)
 
+_tags = {"Name": "k8s-route-table-association-0"}
+_tags.update(common_tags)
 k8s_route_table_association_0 = aws.ec2.RouteTableAssociation(
-    "k8s_route_table_association_0",
+    _tags["Name"],
     subnet_id=k8s_subnet_0.id,
     route_table_id=k8s_route_table_0.id,
 )
 
-k8s_route_table_association_1 = aws.ec2.RouteTableAssociation(
-    "k8s_route_table_association_1",
-    subnet_id=k8s_subnet_1.id,
-    route_table_id=k8s_route_table_0.id,
-)
-
+_tags = {"Name": "k8s-common-sg"}
+_tags.update(common_tags)
 k8s_common_security_group = aws.ec2.SecurityGroup(
-    "k8s_common_security_group",
-    description="k8s Common Security Group",
+    _tags["Name"],
     vpc_id=k8s_vpc.id,
     ingress=[
         aws.ec2.SecurityGroupIngressArgs(
@@ -102,57 +89,22 @@ k8s_common_security_group = aws.ec2.SecurityGroup(
             self=True,
         ),
     ],
-    egress=[],
-    tags={
-        "Name": "k8s_common_security_group",
-        "Stack": stack,
-        "Owner": "hhk7734",
-    },
-)
-
-
-k8s_master_security_group = aws.ec2.SecurityGroup(
-    "k8s_master_security_group",
-    description="k8s Master Security Group",
-    vpc_id=k8s_vpc.id,
-    ingress=[
-        aws.ec2.SecurityGroupIngressArgs(
-            from_port=22,
-            to_port=22,
-            protocol="tcp",
-            cidr_blocks=["0.0.0.0/0"],
-        ),
-        aws.ec2.SecurityGroupIngressArgs(
-            from_port=80,
-            to_port=80,
-            protocol="tcp",
-            cidr_blocks=["0.0.0.0/0"],
-        ),
-        aws.ec2.SecurityGroupIngressArgs(
-            from_port=433,
-            to_port=433,
-            protocol="tcp",
-            cidr_blocks=["0.0.0.0/0"],
-        ),
-    ],
     egress=[
         aws.ec2.SecurityGroupEgressArgs(
             from_port=0,
             to_port=0,
-            protocol="-1",
-            cidr_blocks=["0.0.0.0/0"],
+            protocol="all",
+            self=True,
         ),
     ],
-    tags={
-        "Name": "k8s_master_security_group",
-        "Stack": stack,
-        "Owner": "hhk7734",
-    },
+    tags=_tags,
 )
 
-k8s_worker_security_group = aws.ec2.SecurityGroup(
-    "k8s_worker_security_group",
-    description="k8s Master Security Group",
+
+_tags = {"Name": "k8s-master-sg"}
+_tags.update(common_tags)
+k8s_master_security_group = aws.ec2.SecurityGroup(
+    _tags["Name"],
     vpc_id=k8s_vpc.id,
     ingress=[
         aws.ec2.SecurityGroupIngressArgs(
@@ -176,15 +128,43 @@ k8s_worker_security_group = aws.ec2.SecurityGroup(
             cidr_blocks=["0.0.0.0/0"],
         ),
     ],
-    tags={
-        "Name": "k8s_worker_security_group",
-        "Stack": stack,
-        "Owner": "hhk7734",
-    },
+    tags=_tags,
 )
 
+_tags = {"Name": "k8s-worker-sg"}
+_tags.update(common_tags)
+k8s_worker_security_group = aws.ec2.SecurityGroup(
+    _tags["Name"],
+    vpc_id=k8s_vpc.id,
+    ingress=[
+        aws.ec2.SecurityGroupIngressArgs(
+            from_port=22,
+            to_port=22,
+            protocol="tcp",
+            cidr_blocks=["0.0.0.0/0"],
+        ),
+        aws.ec2.SecurityGroupIngressArgs(
+            from_port=30000,
+            to_port=32767,
+            protocol="tcp",
+            cidr_blocks=["0.0.0.0/0"],
+        ),
+    ],
+    egress=[
+        aws.ec2.SecurityGroupEgressArgs(
+            from_port=0,
+            to_port=0,
+            protocol="-1",
+            cidr_blocks=["0.0.0.0/0"],
+        ),
+    ],
+    tags=_tags,
+)
+
+_tags = {"Name": "k8s-master-iam-role"}
+_tags.update(common_tags)
 k8s_master_iam_role = aws.iam.Role(
-    "k8s_master_iam_role",
+    _tags["Name"],
     assume_role_policy=json.dumps(
         {
             "Version": "2012-10-17",
@@ -198,15 +178,13 @@ k8s_master_iam_role = aws.iam.Role(
             ],
         }
     ),
-    tags={
-        "Name": "k8s_master_iam_role",
-        "Stack": stack,
-        "Owner": "hhk7734",
-    },
+    tags=_tags,
 )
 
+_tags = {"Name": "k8s-worker-iam-role"}
+_tags.update(common_tags)
 k8s_worker_iam_role = aws.iam.Role(
-    "k8s_worker_iam_role",
+    _tags["Name"],
     assume_role_policy=json.dumps(
         {
             "Version": "2012-10-17",
@@ -220,93 +198,16 @@ k8s_worker_iam_role = aws.iam.Role(
             ],
         }
     ),
-    tags={
-        "Name": "k8s_worker_iam_role",
-        "Stack": stack,
-        "Owner": "hhk7734",
-    },
-)
-
-k8s_load_balancer = aws.lb.LoadBalancer(
-    "k8s-load-balancer",
-    load_balancer_type="network",
-    internal=False,
-    subnets=[
-        k8s_subnet_0.id,
-        k8s_subnet_1.id,
-    ],
-    tags={
-        "Name": "k8s-load-balancer",
-        "Stack": stack,
-        "Owner": "hhk7734",
-    },
-)
-
-k8s_lb_http_tg = aws.lb.TargetGroup(
-    "k8s-lb-http-tg",
-    port=80,
-    protocol="TCP",
-    vpc_id=k8s_vpc.id,
-    tags={
-        "Name": "k8s-lb-http-tg",
-        "Stack": stack,
-        "Owner": "hhk7734",
-    },
-)
-
-k8s_lb_http_listener = aws.lb.Listener(
-    "k8s-lb-http-listener",
-    load_balancer_arn=k8s_load_balancer.arn,
-    port=80,
-    protocol="TCP",
-    default_actions=[
-        aws.lb.ListenerDefaultActionArgs(
-            type="forward",
-            target_group_arn=k8s_lb_http_tg.arn,
-        ),
-    ],
-    tags={
-        "Name": "k8s-lb-http-listener",
-        "Stack": stack,
-        "Owner": "hhk7734",
-    },
-)
-
-k8s_lb_https_tg = aws.lb.TargetGroup(
-    "k8s-lb-https-tg",
-    port=433,
-    protocol="TCP",
-    vpc_id=k8s_vpc.id,
-    tags={
-        "Name": "k8s-lb-https-tg",
-        "Stack": stack,
-        "Owner": "hhk7734",
-    },
-)
-
-k8s_lb_https_listener = aws.lb.Listener(
-    "k8s-lb-https-listener",
-    load_balancer_arn=k8s_load_balancer.arn,
-    port=433,
-    protocol="TCP",
-    default_actions=[
-        aws.lb.ListenerDefaultActionArgs(
-            type="forward",
-            target_group_arn=k8s_lb_https_tg.arn,
-        ),
-    ],
-    tags={
-        "Name": "k8s-lb-https-listener",
-        "Stack": stack,
-        "Owner": "hhk7734",
-    },
+    tags=_tags,
 )
 
 with BASE_DIR.joinpath("iam", "policy", "master.json").open() as fd:
     master_policy = fd.read()
 
+_tags = {"Name": "k8s-master-iam-policy"}
+_tags.update(common_tags)
 k8s_master_iam_policy = aws.iam.RolePolicy(
-    "k8s_master_iam_policy",
+    _tags["Name"],
     role=k8s_master_iam_role.id,
     policy=master_policy,
 )
@@ -314,8 +215,10 @@ k8s_master_iam_policy = aws.iam.RolePolicy(
 with BASE_DIR.joinpath("iam", "policy", "worker.json").open() as fd:
     worker_policy = fd.read()
 
+_tags = {"Name": "k8s-worker-iam-policy"}
+_tags.update(common_tags)
 k8s_worker_iam_policy = aws.iam.RolePolicy(
-    "k8s_worker_iam_policy",
+    _tags["Name"],
     role=k8s_worker_iam_role.id,
     policy=worker_policy,
 )
@@ -323,39 +226,35 @@ k8s_worker_iam_policy = aws.iam.RolePolicy(
 with Path.home().joinpath(".ssh", "authorized_keys").open() as fd:
     public_key = fd.readline().strip("\n")
 
+_tags = {"Name": "k8s-key-pair"}
+_tags.update(common_tags)
 k8s_key_pair = aws.ec2.KeyPair(
-    "k8s_key_pair",
-    key_name="k8s_key_pair",
+    _tags["Name"],
+    key_name="k8s-key-pair",
     public_key=public_key,
-    tags={
-        "Name": "k8s_key_pair",
-        "Stack": stack,
-        "Owner": "hhk7734",
-    },
+    tags=_tags,
 )
 
+_tags = {"Name": "k8s-master-instance-profile"}
+_tags.update(common_tags)
 k8s_master_instance_profile = aws.iam.InstanceProfile(
-    "k8s_master_instance_profile",
+    _tags["Name"],
     role=k8s_master_iam_role.name,
-    tags={
-        "Name": "k8s_master_instance_profile",
-        "Stack": stack,
-        "Owner": "hhk7734",
-    },
+    tags=_tags,
 )
 
+_tags = {"Name": "k8s-worker-instance-profile"}
+_tags.update(common_tags)
 k8s_worker_instance_profile = aws.iam.InstanceProfile(
-    "k8s_worker_instance_profile",
+    _tags["Name"],
     role=k8s_worker_iam_role.name,
-    tags={
-        "Name": "k8s_worker_instance_profile",
-        "Stack": stack,
-        "Owner": "hhk7734",
-    },
+    tags=_tags,
 )
 
+_tags = {"Name": "k8s-master-0"}
+_tags.update(common_tags)
 k8s_master_0 = aws.ec2.Instance(
-    "k8s_master_0",
+    _tags["Name"],
     ami="ami-090717c950a5c34d3",  # Ubuntu Server 18.04 LTS
     instance_type="t3.medium",
     associate_public_ip_address=True,
@@ -370,18 +269,14 @@ k8s_master_0 = aws.ec2.Instance(
         k8s_master_security_group.id,
     ],
     key_name=k8s_key_pair.key_name,
-    tags={
-        "Name": "k8s_master_0",
-        "Stack": stack,
-        "Owner": "hhk7734",
-    },
+    tags=_tags,
 )
 
 k8s_worker = []
 for i in range(2):
     k8s_worker.append(
         aws.ec2.Instance(
-            f"k8s_worker_{i}",
+            f"k8s-worker-{i}",
             ami="ami-090717c950a5c34d3",  # Ubuntu Server 18.04 LTS
             instance_type="t3.large",
             associate_public_ip_address=True,
@@ -396,17 +291,13 @@ for i in range(2):
                 k8s_worker_security_group.id,
             ],
             key_name=k8s_key_pair.key_name,
-            tags={
-                "Name": f"k8s_worker_{i}",
-                "Stack": stack,
-                "Owner": "hhk7734",
-            },
+            tags=_tags,
         )
     )
 
 
-pulumi.export("k8s_master_0_public_ip", k8s_master_0.public_ip)
-pulumi.export("k8s_master_0_private_ip", k8s_master_0.private_ip)
+pulumi.export("k8s-master-0-public-ip", k8s_master_0.public_ip)
+pulumi.export("k8s-master-0-private-dns", k8s_master_0.private_dns)
 for i in range(2):
-    pulumi.export(f"k8s_worker_{i}_public_ip", k8s_worker[i].public_ip)
-    pulumi.export(f"k8s_worker_{i}_private_ip", k8s_worker[i].private_ip)
+    pulumi.export(f"k8s-worker-{i}-public-ip", k8s_worker[i].public_ip)
+    pulumi.export(f"k8s-worker-{i}-private-dns", k8s_worker[i].private_dns)
