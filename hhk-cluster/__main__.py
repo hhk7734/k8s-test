1b66c536fea9528d10adb6e5dd09c2b2bed1c756
Hyeonki Hong <hhk7734@gmail.com>
2021-11-06 13:31:19 +0900

    iam: extract profile from main and add role-policy attachments

--- hhk-cluster/__main__.py
+++ hhk-cluster/__main__.py
@@ -3,8 +3,8 @@ from pathlib import Path
 import pulumi
 import pulumi_aws as aws
 
+import iam
 import network
-from iam import policy, role
 
 stack = pulumi.get_stack()
 
@@ -23,86 +23,74 @@ common_tags = {
 with Path.home().joinpath(".ssh", "authorized_keys").open() as fd:
     public_key = fd.readline().strip("\n")
 
-_tags = {"Name": "k8s-key-pair"}
+_tags = {"Name": f"{cluster}-key-pair"}
 _tags.update(common_tags)
 k8s_key_pair = aws.ec2.KeyPair(
     _tags["Name"],
-    key_name="k8s-key-pair",
+    key_name=_tags["Name"],
     public_key=public_key,
     tags=_tags,
 )
 
-_tags = {"Name": "k8s-master-instance-profile"}
-_tags.update(common_tags)
-k8s_master_instance_profile = aws.iam.InstanceProfile(
-    _tags["Name"],
-    role=role.master_node.name,
-    tags=_tags,
-)
 
-_tags = {"Name": "k8s-worker-instance-profile"}
-_tags.update(common_tags)
-k8s_worker_instance_profile = aws.iam.InstanceProfile(
-    _tags["Name"],
-    role=role.worker_node.name,
-    tags=_tags,
-)
+masters = {}
 
-_tags = {"Name": "k8s-master-0"}
-_tags.update(common_tags)
-k8s_master_0 = aws.ec2.Instance(
-    _tags["Name"],
-    ami="ami-090717c950a5c34d3",  # Ubuntu Server 18.04 LTS
-    instance_type="t3.medium",
-    associate_public_ip_address=True,
-    subnet_id=network.subnet[0].id,
-    iam_instance_profile=k8s_master_instance_profile.name,
-    root_block_device=aws.ec2.InstanceRootBlockDeviceArgs(
-        volume_type="gp2",
-        volume_size=50,
-    ),
-    # kubernetes.io/cluster/hhk-cluster
-    # Error syncing load balancer: failed to ensure load balancer:
-    #   Multiple tagged security groups found for instance i-03d3679bc8bdf3374;
-    #   ensure only the k8s security group is tagged;
-    #   the tagged groups were sg-xxx sg-yyy
-    vpc_security_group_ids=[
-        network.common_sg.id,
-        network.elb_sg.id,
-    ],
-    key_name=k8s_key_pair.key_name,
-    tags=_tags,
-)
-
-k8s_workers = []
 for i in range(1):
-    _tags = {"Name": f"k8s-worker-{i}"}
+    _tags = {"Name": f"{cluster}-master-node-{i}"}
     _tags.update(common_tags)
-    k8s_workers.append(
-        aws.ec2.Instance(
-            _tags["Name"],
-            ami="ami-090717c950a5c34d3",  # Ubuntu Server 18.04 LTS
-            instance_type="t3.large",
-            associate_public_ip_address=True,
-            subnet_id=network.subnet[0].id,
-            iam_instance_profile=k8s_worker_instance_profile.name,
-            root_block_device=aws.ec2.InstanceRootBlockDeviceArgs(
-                volume_type="gp2",
-                volume_size=30,
-            ),
-            # Ref: k8s-master-0
-            vpc_security_group_ids=[
-                network.common_sg.id,
-                network.elb_sg.id,
-            ],
-            key_name=k8s_key_pair.key_name,
-            tags=_tags,
-        )
+    masters[i] = aws.ec2.Instance(
+        _tags["Name"],
+        ami="ami-090717c950a5c34d3",  # Ubuntu Server 18.04 LTS
+        instance_type="t3.medium",
+        associate_public_ip_address=True,
+        subnet_id=network.subnet[0].id,
+        iam_instance_profile=iam.master_node_profile.name,
+        root_block_device=aws.ec2.InstanceRootBlockDeviceArgs(
+            volume_type="gp2",
+            volume_size=50,
+        ),
+        # kubernetes.io/cluster/hhk-cluster
+        # Error syncing load balancer: failed to ensure load balancer:
+        #   Multiple tagged security groups found for instance i-03d3679bc8bdf3374;
+        #   ensure only the k8s security group is tagged;
+        #   the tagged groups were sg-xxx sg-yyy
+        vpc_security_group_ids=[
+            network.common_sg.id,
+            network.elb_sg.id,
+        ],
+        key_name=k8s_key_pair.key_name,
+        tags=_tags,
     )
 
 
-pulumi.export("k8s-master-0-public-ip", k8s_master_0.public_ip)
-pulumi.export("k8s-master-0-private-dns", k8s_master_0.private_dns)
-for i, worker in enumerate(k8s_workers):
-    pulumi.export(f"k8s-worker-{i}-public-ip", worker.public_ip)
-    pulumi.export(f"k8s-worker-{i}-private-dns", worker.private_dns)
+workers = {}
+
+for i in range(1):
+    _tags = {"Name": f"{cluster}-worker-node-{i}"}
+    _tags.update(common_tags)
+    workers[i] = aws.ec2.Instance(
+        _tags["Name"],
+        ami="ami-090717c950a5c34d3",  # Ubuntu Server 18.04 LTS
+        instance_type="t3.large",
+        associate_public_ip_address=True,
+        subnet_id=network.subnet[0].id,
+        iam_instance_profile=iam.worker_node_profile.name,
+        root_block_device=aws.ec2.InstanceRootBlockDeviceArgs(
+            volume_type="gp2",
+            volume_size=30,
+        ),
+        # Ref: k8s-master-0
+        vpc_security_group_ids=[
+            network.common_sg.id,
+            network.elb_sg.id,
+        ],
+        key_name=k8s_key_pair.key_name,
+        tags=_tags,
+    )
+
+for i, master in masters.items():
+    pulumi.export(f"{cluster}-master-{i}-public-ip", master.public_ip)
+    pulumi.export(f"{cluster}-master-{i}-private-dns", master.private_dns)
+for i, worker in workers.items():
+    pulumi.export(f"{cluster}-worker-{i}-public-ip", worker.public_ip)
+    pulumi.export(f"{cluster}-worker-{i}-private-dns", worker.private_dns)
--- hhk-cluster/iam/__init__.py
+++ hhk-cluster/iam/__init__.py
@@ -1,1 +1,52 @@
-from . import policy
+from pathlib import Path
+
+import pulumi
+import pulumi_aws as aws
+
+from . import policy, role
+
+stack = pulumi.get_stack()
+
+BASE_DIR = Path(__file__).parent.resolve()
+
+stack = pulumi.get_stack()
+config = pulumi.Config()
+
+cluster = config.require("cluster-name")
+common_tags = {
+    "Stack": stack,
+    f"kubernetes.io/cluster/{cluster}": "owned",  # or "shared"
+    "Manager": config.require("manager"),
+}
+
+_tags = {"Name": f"{cluster}-master-node-role-policy"}
+aws.iam.RolePolicyAttachment(
+    _tags["Name"],
+    policy_arn=policy.master_node.arn,
+    role=role.master_node.name,
+    opts=pulumi.ResourceOptions(parent=role.master_node),
+)
+
+_tags = {"Name": f"{cluster}-master-node-profile"}
+_tags.update(common_tags)
+master_node_profile = aws.iam.InstanceProfile(
+    _tags["Name"],
+    role=role.master_node.name,
+    tags=_tags,
+)
+
+_tags = {"Name": f"{cluster}-worker-node-role-policy"}
+aws.iam.RolePolicyAttachment(
+    _tags["Name"],
+    policy_arn=policy.worker_node.arn,
+    role=role.worker_node.name,
+    opts=pulumi.ResourceOptions(parent=role.worker_node),
+)
+
+_tags = {"Name": f"{cluster}-worker-node-profile"}
+_tags.update(common_tags)
+worker_node_profile = aws.iam.InstanceProfile(
+    _tags["Name"],
+    role=role.worker_node.name,
+    tags=_tags,
+)
